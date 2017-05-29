901ee48b716856e863c4821838207f25cca13ed8
inoridv <daviv_cunha@hotmail.com>
2017-05-28 23:17:54 -0300

    Added parameter handling using reqparse to get function

--- src/api.py
+++ src/api.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 # Initialize the api.
 app = Flask(__name__)
+app.config['BUNDLE_ERRORS'] = True
 api = Api(app)
 
 @api.resource('/v1/occurrences')
@@ -10,7 +11,18 @@ class OccurrencesCrawler(Resource):
     """Handler for this endpoint of the API."""
     def get(self):
         """Return the number of occurrences of the given word in the given URL"""
-        return
+
+        # Parse the url and word parameters.
+        parser = reqparse.RequestParser()
+        parser.add_argument('url', required=True, help='An URL to crawl for the occurrences of the search word.')
+        parser.add_argument('word', required=True, help='A word to search for in the given URL.')
+        args = parser.parse_args()
+        url = args['url']
+        word = args['word']
+
+        # Initialize the function result.
+        result = {}
+        return result
 
 if __name__ == '__main__':
     app.run(debug=True)
